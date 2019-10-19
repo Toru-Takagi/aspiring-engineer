1e6323e0b1faea31ba1c9267404637cd5fe315f2
Toru Takagi <torutakagi@Torus-MacBook.local>
2019-10-20 04:44:44 +0900

    atomsのts化

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import * as React from 'react'
 
 import '../../scss/tag.scss'
 
-export default props => {
+interface Props {
+  bgColor: string
+  color: string
+  itemName: string
+}
+
+export default (props: Props) => {
   const style = {
     backgroundColor: props.bgColor,
     color: props.color,
