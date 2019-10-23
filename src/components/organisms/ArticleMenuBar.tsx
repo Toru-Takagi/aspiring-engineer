1ca355e25dc25a05a8f6bb1abdf77c8a4ecf4fc6
Toru Takagi <torutakagi@Torus-MacBook.local>
2019-10-23 13:17:41 +0900

    ArticleMenuBarの作成

---
+++ src/components/organisms/ArticleMenuBar.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'gatsby'
+
+import { ILikeMapValue, IState } from '../../state/state'
+
+import HomeIcon from '../atoms/icons/HomeIcon'
+import LikeIcon from '../atoms/icons/LikeIcon'
+import NotLikeIcon from '../atoms/icons/NotLikeIcon'
+import TwitterIcon from '../atoms/icons/TwitterIcon'
+import HatenaBookmarkIcon from '../atoms/icons/HatenaBookmarkIcon'
+
+import '../../scss/article-menu-bar.scss'
+
+interface IProps {
+  createNumber: string
+  title: string
+}
+
+export default (props: IProps) => {
+  const dispatch: React.Dispatch<any> = useDispatch()
+  const likeMap: Map<string, ILikeMapValue> = useSelector(
+    (state: IState) => state.likeMap,
+    () => false
+  )
+  const clickLike: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    dispatch({
+      type: 'CLICK_LIKE',
+      likeFlag: e.currentTarget.getAttribute('data-like') === 'true',
+      likeMap: likeMap,
+      createNumber: props.createNumber,
+      title: props.title,
+    })
+  }
+  return (
+    <div id='menu-bar-bg'>
+      <div id='menu-bar'>
+        <Link to='/'>
+          <div className='menu-bar-item'>
+            <HomeIcon />
+            <span>Home</span>
+          </div>
+        </Link>
+        <div
+          className='menu-bar-item'
+          data-like={
+            likeMap.get(props.createNumber) !== undefined ? true : false
+          }
+          onClick={clickLike}
+        >
+          {likeMap.get(props.createNumber) !== undefined ? (
+            <LikeIcon />
+          ) : (
+            <NotLikeIcon />
+          )}
+          <span>Like</span>
+        </div>
+        <div className='menu-bar-item twitter-icon'>
+          <TwitterIcon />
+          <span>Twitter</span>
+        </div>
+        <div className='menu-bar-item'>
+          <HatenaBookmarkIcon />
+          <span>Hatena</span>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/templates/Article.tsx
+++ src/templates/Article.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
-import { useSelector, useDispatch } from 'react-redux'
 
 import Converter from '../mixins/converter'
-import { IState, ILikeMapValue } from '../state/state'
 import { IArticleAndCategory } from '../model/allContentfulArticle'
 
 import Layout from './DefaultLayout'
+import ArticleMenuBar from '../components/organisms/ArticleMenuBar'
 import Tag from '../components/atoms/Tag'
-import HomeIcon from '../components/atoms/icons/HomeIcon'
-import LikeIcon from '../components/atoms/icons/LikeIcon'
-import NotLikeIcon from '../components/atoms/icons/NotLikeIcon'
-import TwitterIcon from '../components/atoms/icons/TwitterIcon'
-import HatenaBookmarkIcon from '../components/atoms/icons/HatenaBookmarkIcon'
 
 import '../scss/article.scss'
 import '../scss/prism.scss'
@@ -26,21 +20,7 @@ interface IProps {
 
 export default (props: IProps) => {
   const data: IArticleAndCategory = props.pageContext.data
-  const likeMap: Map<string, ILikeMapValue> = useSelector(
-    (state: IState) => state.likeMap
-  )
-  const dispatch: React.Dispatch<any> = useDispatch()
-  const clickLike: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    dispatch({
-      type: 'CLICK_LIKE',
-      likeFlag: e.currentTarget.getAttribute('data-like') === 'true',
-      likeMap: likeMap,
-      createNumber: data.createNumber.toString(),
-      title: data.title,
-    })
-  }
+
   return (
     <Layout>
       <div id='article'>
@@ -53,40 +33,10 @@ export default (props: IProps) => {
             </span>
           </div>
         </header>
-        <div id='menu-bar-bg'>
-          <div id='menu-bar'>
-            <Link to='/'>
-              <div className='menu-bar-item'>
-                <HomeIcon />
-                <span>Home</span>
-              </div>
-            </Link>
-            <div
-              className='menu-bar-item'
-              data-like={
-                likeMap.get(data.createNumber.toString()) !== undefined
-                  ? true
-                  : false
-              }
-              onClick={clickLike}
-            >
-              {likeMap.get(data.createNumber.toString()) !== undefined ? (
-                <LikeIcon />
-              ) : (
-                <NotLikeIcon />
-              )}
-              <span>Like</span>
-            </div>
-            <div className='menu-bar-item twitter-icon'>
-              <TwitterIcon />
-              <span>Twitter</span>
-            </div>
-            <div className='menu-bar-item'>
-              <HatenaBookmarkIcon />
-              <span>Hatena</span>
-            </div>
-          </div>
-        </div>
+        <ArticleMenuBar
+          createNumber={String(data.createNumber)}
+          title={data.title}
+        />
         <div
           className='article-main'
           dangerouslySetInnerHTML={{
