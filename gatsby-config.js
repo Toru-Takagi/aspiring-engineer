574aaa993151ca699af04315e9e69d4426d3b5b8
Toru Takagi <torutakagi@Torus-MacBook.local>
2019-11-17 17:49:54 +0900

    GoogleAnalytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,6 +135,12 @@ module.exports = {
     },
     `gatsby-plugin-twitter`,
     `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-152704650-1`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
